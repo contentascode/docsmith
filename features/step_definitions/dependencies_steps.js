22e0a3942461e5cafc92629bd23177cf110de5c5
Jun Matsushita <jun@iilab.org>
2016-03-11 20:03:16 +0100

    Run cucumber tests. Pending steps. #2

---
+++ features/step_definitions/dependencies_steps.js
@@ -0,0 +1,13 @@
+module.exports = function () {
+
+  this.Given(/^An environment without ruby\.$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Given(/^An environment without the travis command\.$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+};
