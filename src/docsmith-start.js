9c622ece3108e236e910eafce75e05ffbe17bfa9
Jun Matsushita <jun@iilab.org>
2017-10-16 15:41:20 +0200

    Added no-warning flag

--- src/docsmith-start.js
+++ src/docsmith-start.js
@@ -16,6 +16,7 @@ program
   .option('-s, --source [path]', '[migrate] Source folder path]')
   .option('--baseurl [baseurl]', 'Set site.baseurl metadata value.')
   .option('-n, --no-watch', 'Do not watch content folder and serve on local server. Watches by default.')
+  .option('-w, --no-warning', 'Do not display transclusion warnings.')
   .option('-d, --debug', 'Enable /debug-ui url for debugging pipeline.')
   .option('-c, --clean', 'Cleanup build directory before build.')
   .option('-l, --link', 'For development purposes. Link local packages.')
@@ -25,15 +26,14 @@ program
 
 program.parse(process.argv);
 
-const { link, source, baseurl, watch, clean, debug: dbg } = program;
-
 if (typeof workspace === 'undefined') workspace = 'toolkit';
+const { link, source, baseurl, watch, clean, debug: dbg, warning } = program;
 
 // check if we could resolve the config.
 if (config) {
   debug('config', config);
   // called from a content as code instance, initialise from the instance configuration
-  start.run({ workspace, config, link, source, watch, clean, dbg, baseurl });
+  start.run({ workspace, config, link, source, watch, clean, dbg, baseurl, warning });
 } else {
   console.warn('Could not find config. Aborting start. Please contact the developer');
 }
